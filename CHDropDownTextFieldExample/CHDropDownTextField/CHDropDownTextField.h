42487b8ac8da5f386926e4777bba6bd84a86e4d8
Yuichi Fujiki <yuichi@chaione.com>
2017-04-28 15:49:22 -0500

    Added Framework scheme as a shared scheme.

---
+++ CHDropDownTextFieldExample/CHDropDownTextField/CHDropDownTextField.h
@@ -0,0 +1,19 @@
+//
+//  CHDropDownTextField.h
+//  CHDropDownTextField
+//
+//  Created by Yuichi Fujiki on 4/28/17.
+//  Copyright © 2017 ChaiOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHDropDownTextField.
+FOUNDATION_EXPORT double CHDropDownTextFieldVersionNumber;
+
+//! Project version string for CHDropDownTextField.
+FOUNDATION_EXPORT const unsigned char CHDropDownTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHDropDownTextField/PublicHeader.h>
+
+
